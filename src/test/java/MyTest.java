ac8e62a92624ce3c5bccad334bc381a59a27e5d9
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-17 16:09:43 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,32 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class MyTest {
     @Test
-    void name() {
-
+    public void countPositivesSumNegatives_BasicTest() {
+        int[] expectedResult = new int[] {10, -65};
+        assertArrayEquals(expectedResult, Kata.countPositivesSumNegatives(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15}));
     }
-}
+
+    @Test
+    public void countPositivesSumNegatives_InputWithZeroes() {
+        int[] expectedResult = new int[] {8, -50};
+        assertArrayEquals(expectedResult, Kata.countPositivesSumNegatives(new int[] {0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14}));
+    }
+
+    private static class Kata {
+        public static int[] countPositivesSumNegatives(int[] input) {
+            {
+
+                if (input == null || input.length == 0) return new int[] {};
+                int count = 0,sum = 0;
+                for (int i : input) {
+                    if (i > 0) count ++;
+                    if (i < 0) sum += i;
+                }
+                return new int[] {count,sum};
+            }
+        }
+        }
+    }
